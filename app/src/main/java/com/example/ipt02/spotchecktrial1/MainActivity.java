7838c868070ca98f8a90a44e5e481bccb73d56c5
abhinav-adtechs <abhinav.adtechs@gmail.com>
2016-07-01 16:32:41 +0530

    fix

--- app/src/main/java/com/example/ipt02/spotchecktrial1/MainActivity.java
+++ app/src/main/java/com/example/ipt02/spotchecktrial1/MainActivity.java
@@ -1,79 +1,20 @@
 package com.example.ipt02.spotchecktrial1;
 
-import android.app.Fragment;
-import android.content.Context;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.ActionBarActivity;
+
 import android.os.Bundle;
-import android.util.AttributeSet;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       setContentView(R.layout.activity_main);
-//
-//        if(savedInstanceState==null)
-//        {
-//            getSupportFragmentManager().beginTransaction().add(R.id.fragment, new MainActivityFragment()).commit();
-//        }
-//
-        FragmentManager fm = getSupportFragmentManager();
-        MainActivityFragment fragment = (MainActivityFragment) fm.findFragmentByTag("main_fragment");
-        if (fragment == null) {
-            FragmentTransaction ft = fm.beginTransaction();
-            fragment =new MainActivityFragment();
-            ft.add(android.R.id.content,fragment,"main_fragment");
-            ft.commit();
-        }
 
-    }
+        setContentView(R.layout.activity_main);
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
-
-//if(savedInstanceState == null){
-//
-//            getSupportFragmentManager().beginTransaction()
-//                    .replace(R.id.fragment,new MainActivityFragment())
-//                    .addToBackStack(null).commit();
-//        }
-//        getSupportFragmentManager().addOnBackStackChangedListener(new FragmentManager.OnBackStackChangedListener() {
-//            @Override
-//            public void onBackStackChanged() {
-//                int backCount = getSupportFragmentManager().getBackStackEntryCount();
-//                if(backCount==0) finish();
-//            }
-//        });
-
-
